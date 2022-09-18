08a44665039e6132fc6e03e8f2be3810fa392392
peder frang <peder.frang@gmail.com>
2022-09-18 11:36:46 +0200

    push

--- ui-kit/search-loader.tsx
+++ ui-kit/search-loader.tsx
@@ -3,17 +3,18 @@ import styled, { keyframes } from "styled-components";
 
 const AnimationLine = keyframes`
 0% { width: 0%}
-25% { width: 25%}
-50% { width: 50%; margin-left: 25%}
+25% { width: 25%;}
+50% { width: 50%; margin-left: 25%; box-shadow: 0px 100px 30px 100px rgba(0, 0, 0, 0.2);}
 75% { width: 50%; margin-left: 50%}
 100% { width: 1%; margin-left: 99%}
 `
 
 const AnimationSun = keyframes`
 0% { width: 0%;}
-25% { width: 25%; top: 20%;}
-50% { width: 50%; margin-left: 25%; top: 0%;}
-75% { width: 50%; margin-left: 50%; top: 10%;}
+/* 13% { width: 13%; top: 25%;}
+25% { width: 25%; top: 20%;} */
+50% { width: 50%; margin-left: 25%; top: 2%;}
+/* 75% { width: 50%; margin-left: 50%; top: 10%;} */
 100% { width: 1%; margin-left: 99%; top: 30%;}
 `
 
@@ -34,7 +35,6 @@ const StyledLine = styled.div`
   position: absolute;
   top: 50%;
   background-color: black;
-  box-shadow: 0px 100px 30px 100px rgba(0, 0, 0, 0.2);
   height: 2px;
   width: 0%;
   animation: ${AnimationLine} 5s infinite linear;
