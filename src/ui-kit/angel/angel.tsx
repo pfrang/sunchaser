a4ec39ec109f31e4d7274a98dbe60597465518d9
peder frang <peder.frang@gmail.com>
2023-03-26 16:10:00 +0200

    bug-angel-white

--- src/ui-kit/angel/angel.tsx
+++ src/ui-kit/angel/angel.tsx
@@ -30,7 +30,7 @@ export const Angel: FC<Direction> = ({ direction, ...props }: Direction) => {
         <AngelStyle>
           <path
             d="M23.245 4l-11.245 14.374-11.219-14.374-.781.619 12 15.381 12-15.391-.755-.609z"
-            stroke={`${theme.white}`}
+            stroke={`${theme.color.white}`}
           />
         </AngelStyle>
       );
