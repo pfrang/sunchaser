7b205ec411d569961633f7200b93b1c2ef986bfe
AndersLN <w.anders.nordgaard@gmail.com>
2023-05-09 15:55:57 +0200

    sql filter logiq for populating location names

--- azure-function/src/python/Sql_connection/GoogleLocations.py
+++ azure-function/src/python/Sql_connection/GoogleLocations.py
@@ -13,32 +13,50 @@ conn=pyodbc.connect('DRIVER='+driver+';SERVER=tcp:'+server+';PORT=1433;DATABASE=
 cursor = conn.cursor()
 
 #Connecting to master sql table to collect all lat, lon
-sql="SELECT lat,lon FROM coordinates_all where country=?"
+#Continue on the last known lat or select all if table is empty
+sql='''
+
+IF EXISTS (SELECT TOP 1 lat FROM coordinates_locationdata_google ORDER BY lat DESC)
+    SELECT * FROM coordinates_all
+    WHERE lat >= 
+        (SELECT TOP 1 lat FROM coordinates_locationdata_google ORDER BY lat DESC)
+    AND country = ?
+    ORDER BY lat, lon
+ELSE
+    SELECT * FROM coordinates_all
+    WHERE country = ?
+    ORDER BY lat,lon
+
+'''
 
 # Get data from table
-cursor.execute(sql,"Norway")
+cursor.execute(sql,"Norway","Norway")
 data = cursor.fetchall()
 
 #add data from sql to pandas 
 df = pd.DataFrame(data)
 conn.commit()
 count=0
-#loop each lat lon pair to run YR.api for sunset and sunrise
+#find the first lat so that previous records for this lat can be deleted (restarted)
+first_lat =float(str(df.iloc[0][0]).split(",")[0][1:])
+#loop each lat lon pair
 for index,row in df.iterrows():
     count+=1
     lat=float(str(row[0]).split(",")[0][1:])
     lon=float(str(row[0]).split(",")[1][:-1])
     
+    if lat==first_lat:
+        print("lat: {lat}, lon: {lon} is deleted")
+        #delete previous records for the specific location and add new data
+        cursor.execute('''
+                     DELETE FROM coordinates_locationdata_google 
+                     WHERE lat=? and lon=?
+                    ''',lat,lon)
+
+        conn.commit()
+    
     LocationDataFrame=googleLocationName(lat,lon)
 
-    #delete previous records for the specific location and add new data
-    # cursor.execute('''
-    #             DELETE FROM coordinates_locationdata_google 
-    #             WHERE lat=? and lon=?
-    #            ''',lat,lon)
-
-    conn.commit()
-    
     #add the new data to the table
     for index,location in LocationDataFrame.iterrows():
         cursor.execute('''
