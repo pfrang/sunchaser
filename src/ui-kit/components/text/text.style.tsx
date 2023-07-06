dc10768e7330f133af03cc248f5dc44b3e8a69eb
peder frang <peder.frang@gmail.com>
2023-07-06 23:57:54 +0200

    eslint-stuff

--- src/ui-kit/components/text/text.style.tsx
+++ src/ui-kit/components/text/text.style.tsx
@@ -19,96 +19,96 @@ export const TextWrapper = styled.p<{
   ${space};
 
   ${variant({
-  variants: {
-    regular: {
-      fontFamily: "SpaceMono",
-      fontWeight: "400",
-      fontSize: ["12px", "16px", "20px"],
-      lineHeight: ["28px", "40px"],
-    },
-    intro: {
-      fontFamily: "SpaceMono",
-      fontWeight: "400",
-      fontSize: ["18px", "24px"],
-      lineHeight: ["28px", "40px"],
-    },
-    ["subtitle-large"]: {
-      fontFamily: "ProximaNova",
-      fontWeight: "600",
-      fontSize: ["18px", "20px", "22px"],
-      lineHeight: ["28px", "30px", "32px"],
-    },
-    ["body-small"]: {
-      fontFamily: "ProximaNova",
-      fontWeight: "400",
-      fontSize: ["12px", "16px", "18px"],
-      lineHeight: ["24px", "24px", "28px"],
-    },
-    ["body-small-bold"]: {
-      fontFamily: "ProximaNova",
-      fontWeight: "600",
-      fontSize: ["12px", "16px", "18px"],
-      lineHeight: ["24px", "24px", "28px"],
-    },
-    ["body-large"]: {
-      fontFamily: "ProximaNova",
-      fontWeight: "400",
-      fontSize: ["18px", "20px", "22px"],
-      lineHeight: ["28px", "30px", "32px"],
-    },
-    ["body-large-bold"]: {
-      fontFamily: "ProximaNova",
-      fontWeight: "600",
-      fontSize: ["18px", "20px", "22px"],
-      lineHeight: ["28px", "30px", "32px"],
-    },
-    ["subtitle-small"]: {
-      fontFamily: "ProximaNova",
-      fontWeight: "400",
-      fontSize: "16px",
-      lineHeight: "24px",
-    },
-    ["subtitle-small-bold"]: {
-      fontFamily: "ProximaNova",
-      fontWeight: "600",
-      fontSize: "16px",
-      lineHeight: "24px",
-    },
-    search: {
-      fontFamily: "SpaceMono",
-      fontWeight: "400",
-      fontSize: "15px",
-      lineHeight: "24px",
-    },
-    caption: {
-      fontFamily: "SpaceMono",
-      fontWeight: "400",
-      fontSize: "12px",
-      lineHeight: "16px",
-    },
-    ["caption-large"]: {
-      fontFamily: "SpaceMono",
-      fontWeight: "400",
-      fontSize: "16px",
-      lineHeight: "24px",
-    },
-    overline: {
-      fontFamily: "SpaceMono",
-      fontWeight: "700",
-      fontSize: "11px",
-      lineHeight: "16px",
+    variants: {
+      regular: {
+        fontFamily: "SpaceMono",
+        fontWeight: "400",
+        fontSize: ["12px", "16px", "20px"],
+        lineHeight: ["28px", "40px"],
+      },
+      intro: {
+        fontFamily: "SpaceMono",
+        fontWeight: "400",
+        fontSize: ["18px", "24px"],
+        lineHeight: ["28px", "40px"],
+      },
+      ["subtitle-large"]: {
+        fontFamily: "ProximaNova",
+        fontWeight: "600",
+        fontSize: ["18px", "20px", "22px"],
+        lineHeight: ["28px", "30px", "32px"],
+      },
+      ["body-small"]: {
+        fontFamily: "ProximaNova",
+        fontWeight: "400",
+        fontSize: ["12px", "16px", "18px"],
+        lineHeight: ["24px", "24px", "28px"],
+      },
+      ["body-small-bold"]: {
+        fontFamily: "ProximaNova",
+        fontWeight: "600",
+        fontSize: ["12px", "16px", "18px"],
+        lineHeight: ["24px", "24px", "28px"],
+      },
+      ["body-large"]: {
+        fontFamily: "ProximaNova",
+        fontWeight: "400",
+        fontSize: ["18px", "20px", "22px"],
+        lineHeight: ["28px", "30px", "32px"],
+      },
+      ["body-large-bold"]: {
+        fontFamily: "ProximaNova",
+        fontWeight: "600",
+        fontSize: ["18px", "20px", "22px"],
+        lineHeight: ["28px", "30px", "32px"],
+      },
+      ["subtitle-small"]: {
+        fontFamily: "ProximaNova",
+        fontWeight: "400",
+        fontSize: "16px",
+        lineHeight: "24px",
+      },
+      ["subtitle-small-bold"]: {
+        fontFamily: "ProximaNova",
+        fontWeight: "600",
+        fontSize: "16px",
+        lineHeight: "24px",
+      },
+      search: {
+        fontFamily: "SpaceMono",
+        fontWeight: "400",
+        fontSize: "15px",
+        lineHeight: "24px",
+      },
+      caption: {
+        fontFamily: "SpaceMono",
+        fontWeight: "400",
+        fontSize: "12px",
+        lineHeight: "16px",
+      },
+      ["caption-large"]: {
+        fontFamily: "SpaceMono",
+        fontWeight: "400",
+        fontSize: "16px",
+        lineHeight: "24px",
+      },
+      overline: {
+        fontFamily: "SpaceMono",
+        fontWeight: "700",
+        fontSize: "11px",
+        lineHeight: "16px",
 
-      textTransform: "uppercase",
-      letterSpacing: "0.02em",
+        textTransform: "uppercase",
+        letterSpacing: "0.02em",
+      },
+      button: {
+        fontFamily: "SpaceMono",
+        fontWeight: "700",
+        fontSize: "16px",
+        lineHeight: "24px",
+      },
     },
-    button: {
-      fontFamily: "SpaceMono",
-      fontWeight: "700",
-      fontSize: "16px",
-      lineHeight: "24px",
-    },
-  },
-})}
+  })}
   ${typography};
   ${layout};
 `;
