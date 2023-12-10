eb21e7713e2b1bfc2398bafa80b13080f899292c
Peder Frang <peder.frang@gmail.com>
2023-12-10 15:01:31 +0100

    set-headers-options

--- src/pages/api/azure-function/coordinates/index.endpoint.ts
+++ src/pages/api/azure-function/coordinates/index.endpoint.ts
@@ -17,6 +17,18 @@ export interface CoordinatesNextApiResponse
 
 export const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   switch (req.method) {
+    case "OPTIONS":
+      res.setHeader("Access-Control-Allow-Origin", "*");
+      res.setHeader(
+        "Access-Control-Allow-Methods",
+        "GET, POST, DELETE, PATCH, PUT, OPTIONS",
+      );
+      res.setHeader(
+        "Access-Control-Allow-Headers",
+        "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
+      );
+      res.status(200).end();
+      return;
     case "POST":
       return await handlePost(req, res);
     default:
