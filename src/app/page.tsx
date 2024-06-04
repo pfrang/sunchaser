ff2a95ec8a1bd3b08e573f09545dcdc5bf34443f
Peder Frang <peder.frang@gmail.com>
2024-06-04 23:57:02 +0200

    start removing settings expandable

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,12 @@
 import { AppConfig } from "app-config";
 import { IosInstallPrompt } from "pwa/ios-install-prompt";
 import { Suspense } from "react";
-import mapboxgl from "mapbox-gl";
 
 import { Spinner } from "../ui-kit/spinner/spinner";
 
 import Router from "./components/map-renderer";
 import { Header } from "./header";
 import { Footer } from "./components/footer";
-import { SettingsExpandable } from "./components/settings-expandable";
 
 export default function Page() {
   const mapboxKey = new AppConfig().mapBox.key;
@@ -16,14 +14,13 @@ export default function Page() {
   // if (!session) throw new Error("Session not found");
   return (
     <>
-      <div className="flex h-dvh w-[100%] flex-col">
+      <div className="flex h-dvh w-full flex-col">
         <Suspense fallback={<Spinner />}>
           <Header />
           <main className="h-full">
             <Router mapboxKey={mapboxKey} />
           </main>
 
-          <SettingsExpandable />
           <Footer />
           <IosInstallPrompt />
         </Suspense>
