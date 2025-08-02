9acddc5540818e1ef73abb1aafb86f2927723c06
Peder Frang <peder.frang@gmail.com>
2025-08-02 11:33:24 +0200

    Tr to improve geo location err handling for native

--- src/app/hooks/use-user-location.ts
+++ src/app/hooks/use-user-location.ts
@@ -1,3 +1,4 @@
+import { Capacitor } from "@capacitor/core";
 import { Geolocation } from "@capacitor/geolocation";
 import { UserLocation } from "app/api/azure-function/coordinates/coordinates-api-client/coordinates-api-response-schema";
 import { useState, useEffect } from "react";
@@ -6,34 +7,45 @@ export const useUserLocation = () => {
   const [userLocation, setLocation] = useState<UserLocation | null>(null);
   const [error, setError] = useState<string | null>(null);
 
-  useEffect(() => {
-    const geoLocation = async () => {
-      try {
-        const geo = Geolocation;
-        if (!geo) {
-          setError("Geolocation is not supported");
-          return;
-        }
-        const { longitude, latitude } = (await geo.getCurrentPosition()).coords;
-        if (!longitude || !latitude) {
-          setError("Unable to retrieve user location");
-          return;
-        }
-
-        const userLocation = {
-          longitude,
-          latitude,
-        };
-
-        setLocation(userLocation);
-        setError(null);
-      } catch (err) {
-        setError("Failed to get location");
-        console.error("Geolocation error:", err);
+  const requestLocation = async () => {
+    try {
+      const geo = Geolocation;
+      if (!geo) {
+        setError("Geolocation is not supported");
+        return;
       }
-    };
-    geoLocation();
+      const { longitude, latitude } = (await geo.getCurrentPosition()).coords;
+      if (!longitude || !latitude) {
+        setError("Unable to retrieve user location");
+        return;
+      }
+
+      const userLocation = {
+        longitude,
+        latitude,
+      };
+
+      setLocation(userLocation);
+      setError(null);
+    } catch (err) {
+      setError("Failed to get location");
+      console.error("Geolocation error:", err);
+    }
+  };
+
+  useEffect(() => {
+    if (Capacitor.isNativePlatform()) {
+      Geolocation.requestPermissions()
+        .then(() => {
+          requestLocation();
+        })
+        .catch((err) => {
+          setError("Permission denied for geolocation");
+          console.error("Geolocation permission error:", err);
+        });
+    }
+    requestLocation();
   }, []);
 
-  return { userLocation, error };
+  return { userLocation, error, requestLocation };
 };
