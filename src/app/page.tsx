5c8fcfa4d9c024893ecd502c00da1de2f3e6c483
Peder Frang <peder.frang@gmail.com>
2025-10-04 14:31:02 +0200

    Remove dynamic page and tips & tricks

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,7 +9,7 @@ import { Footer } from "./components/sunchaser/components/footer/footer";
 import { RightButtonsWrapper } from "./components/sunchaser/components/filter-buttons/form";
 import { SettingsButton } from "./components/settings-button";
 
-export const revalidate = 0; // Disable caching, always fetch a fresh version
+// export const revalidate = 0; // Disable caching, always fetch a fresh version
 
 export default function Page() {
   const mapboxKey = new AppConfig().mapBox.key;
